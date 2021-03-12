95d5a0c551c9c7b895bdcda772d3da8333fc779a
Skips <dthecsguy@gmail.com>
2021-03-11 23:32:04 -0800

    ticking clock

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//import './iPortfolioRough/assets/css/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const name = 'Josh Perez';
-const element = <h1>Hello, {name}</h1>;
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
 
-ReactDOM.render(
-	element,
-	document.getElementById('root')
-);
+setInterval(tick, 1000);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
