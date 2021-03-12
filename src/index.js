106546e4e7f7509b7227f71de67a7c7ce0ff2cf4
Skips <dthecsguy@gmail.com>
2021-03-11 22:54:05 -0800

    hello world

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	element,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
